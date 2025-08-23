53d1d62bcb47369b1606c1a9c3fa7b210f537363
higor.losilva <higor.losilva@OSA0716338W11-1>
2025-08-23 09:36:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ficha
+{
+
+    class Program
+    {
+        // Aula de hoje do Senac 23/08/2025
+        static void Main(string[] args)
+        {
+            // Declaração de variáveis
+            string nome = "Higor Oliveira";
+            // Declaração de variáveis adicionais
+            int idade = 25;
+            // Declaração de variáveis adicionais
+            string curso = ("Engenharia de Software");
+            // Declaração de variáveis adicionais
+            string nMatricula = ("2023002");
+            // Declaração de variáveis adicionais
+
+
+
+            // Exibição do cabeçalho
+            Console.WriteLine("---------------FICHA DE INSCRIÇÃO---------------\n");
+            // Exibição das variáveis
+            Console.WriteLine($"Nome:{nome}");
+            // Exibição das variáveis
+            Console.WriteLine($"Idade:{idade}");
+            // Exibição das variáveis adicionais
+            Console.WriteLine($"Curso:{curso}");
+            // Exibição das variáveis adicionais
+            Console.WriteLine($"Nº Matrícula:{nMatricula}");    
+
+        }
+    }
+}
